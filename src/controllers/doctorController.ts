0247aff0d2adb997eff108b3b00991fb5587f2a5
FernandoAragonMoreno <vehiculos.aragon@gmail.com>
2023-12-11 22:54:11 -0500

    se instala (npm install pg)

--- src/controllers/doctorController.ts
+++ src/controllers/doctorController.ts
@@ -14,3 +14,23 @@ export const getDoctores = async (
     res.status(500).send("Error del servidor");
   }
 };
+
+/*
+import { Request, Response } from "express";
+import { Pool, QueryResult } from "pg";
+import { pool } from "../database";
+
+export const getDoctores = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const client = await pool; // Obtener la conexión del pool (aquí estamos extrayendo la conexión real)
+
+    const result: QueryResult = await client.query("SELECT * FROM doctores");
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error("Error al obtener doctores:", error);
+    res.status(500).send("Error del servidor");
+  }
+};*/
